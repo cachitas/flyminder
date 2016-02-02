5b862723619cc6e769b3a113599c760c774bf855
Hugo Cachitas <hcachitas@gmail.com>
2016-02-02 14:37:08 +0000

    Point wsgi to development settings

--- flyminder/wsgi.py
+++ flyminder/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flyminder.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flyminder.settings.development")
 
 application = get_wsgi_application()
